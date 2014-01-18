166fceb79e124e149e1024e2ede8fbde289eb1e2
Jeremy Meyer <jjman505@gmail.com>
2014-01-17 20:14:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideallist::Application.config.secret_key_base = '5ad2466a51b426f90bcdf1c954f0c08936664ee8284360c91df53748b72eb30e39898e47215504f963b0e8f905c19d8f72749b9507c8c42a257dc569a6a2e316'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideallist::Application.config.session_store :cookie_store, key: '_ideallist_session'
